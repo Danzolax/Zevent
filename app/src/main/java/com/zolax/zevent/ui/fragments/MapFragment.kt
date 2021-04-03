eabc46f2804ea2805d320b3c58ea912d67eed980
Danzolax <danzolax@gmail.com>
2021-04-03 21:22:29 +0300

    добавил обновление локации

--- app/src/main/java/com/zolax/zevent/ui/fragments/MapFragment.kt
+++ app/src/main/java/com/zolax/zevent/ui/fragments/MapFragment.kt
@@ -4,9 +4,16 @@ package com.zolax.zevent.ui.fragments
 import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.annotation.SuppressLint
-import android.location.Location
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -21,6 +28,7 @@ import kotlinx.android.synthetic.main.fragment_map.*
 import pub.devrel.easypermissions.EasyPermissions
 import javax.inject.Inject
 
+
 @AndroidEntryPoint
 class MapFragment : Fragment(R.layout.fragment_map), EasyPermissions.PermissionCallbacks{
 
@@ -29,35 +37,70 @@ class MapFragment : Fragment(R.layout.fragment_map), EasyPermissions.PermissionC
     @Inject
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
-    @SuppressLint("MissingPermission")
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         requestPermission()
+        launchMap(savedInstanceState)
+
+
+    }
+
+
+
+    private fun launchMap(savedInstanceState: Bundle?){
         mapView?.onCreate(savedInstanceState)
         moveCameraToUserLocation()
         mapUISettings()
         initButtons()
         mapView?.getMapAsync { googleMap ->
             map = googleMap
-            map?.isMyLocationEnabled = true
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+              requestPermission()
+            } else{
+                map?.isMyLocationEnabled = true
+            }
         }
     }
-
     private fun initButtons() {
         fab_location.setOnClickListener {
             moveCameraToUserLocation()
         }
     }
 
-    @SuppressLint("MissingPermission")
+    @SuppressLint("MissingPermission", "NewApi")
     private fun moveCameraToUserLocation() {
-        fusedLocationProviderClient.lastLocation.addOnSuccessListener {
-            map?.moveCamera(
-                CameraUpdateFactory.newLatLngZoom(
-                    LatLng(it.latitude, it.longitude), MAP_CAMERA_ZOOM
+        val locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (locationManager.isLocationEnabled){
+            fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+                map?.moveCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                        LatLng(it.latitude, it.longitude), MAP_CAMERA_ZOOM
+                    )
                 )
-            )
+            }
         }
+        else{
+            buildAlertMessageNoLocationService()
+        }
+
+    }
+
+    private fun buildAlertMessageNoLocationService() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        builder.setCancelable(false)
+            .setMessage("Включите GPS для отслеживания вашего местоположения")
+            .setPositiveButton("Включить"
+            ) { _, _ -> startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)) }
+        val alert: AlertDialog = builder.create()
+        alert.show()
     }
 
 
@@ -87,7 +130,7 @@ class MapFragment : Fragment(R.layout.fragment_map), EasyPermissions.PermissionC
 
     override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
         if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
-            Snackbar.make(requireView(), "Помянем дурочка", Snackbar.LENGTH_LONG).show()
+            Snackbar.make(requireView(), "Отслеживание включено", Snackbar.LENGTH_LONG).show()
         } else {
             requestPermission()
         }
